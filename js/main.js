59bd2d77810b76dd18cca1e8bf566fe56eabee70
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-10 14:28:43 -0700

    i think the static elements are working

--- js/main.js
+++ js/main.js
@@ -1,59 +1,6 @@
 $(function () {
     'use strict';
 
-    // Graph margin settings
-    var margin = {
-        top: 10,
-        right: 10,
-        bottom: 150,
-        left: 60
-    };
-
-    // SVG width and height
-    var width = 960;
-    var height = 500;
-
-    // Graph width and height - accounting for margins
-    var drawWidth = width - margin.left - margin.right;
-    var drawHeight = height - margin.top - margin.bottom;
-
-    /************************************** Create chart wrappers ***************************************/
-    // Create a variable `svg` in which you store a selection of the element with id `viz`
-    // Set the width and height to your `width` and `height` variables
-    let svg = d3.select('#viz')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    // Append a `g` element to your svg in which you'll draw your bars. Store the element in a variable called `g`, and
-    // Transform the g using `margin.left` and `margin.top`
-    let g = svg.append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-
-    let xAxisLabel = svg.append('g')
-        .attr('transform', `translate(${margin.left}, ${margin.top + drawHeight})`)
-        .attr('class', 'axis')
-        // .call(xAxis);
-
-    let xAxisText = svg.append('text')
-        .attr('transform', `translate(${margin.left + drawWidth / 2}, ${margin.top + drawHeight + 40})`)
-        .attr('class', 'axis-label')
-        .text('X AXIS TEST');
-
-
-    let yAxisLabel = svg.append('g')
-        .attr('class', 'axis')
-        .attr('transform', `translate(${margin.left}, ${margin.top})`)
-        // .call(yAxis);
-
-
-    // Using the same pattern as your x-axis, append a text element to label your y axis
-    // Set its class to 'axis-label', and set the text to "Count"
-    let yAxisText = svg.append('text')
-        .attr('transform', `translate(${margin.left - 40}, ${margin.top + drawHeight / 2}) rotate(-90)`)
-        .attr('class', 'axis-label')
-        .text('Y AXIS TEXT');
 
     d3.csv('data/sp500.csv', function (error, data) {
 
--- reusable-time-series/js/time-series.js
+++ reusable-time-series/js/time-series.js
@@ -2,17 +2,65 @@ class TimeSeriesChart {
     constructor() {
         this._width = 200;
         this._height = 100;
+        this._xScale = d3.scaleLinear();
+        this._yScale = d3.scaleLinear();
+        this._xTitle = 'X AXIS TITLE';
+        this._yTitle = 'Y AXIS TITLE';
+
+        // Graph margin settings
+        this._margin = {
+            top: 10,
+            right: 10,
+            bottom: 150,
+            left: 60
+        };
     }
 
     // render chart here
     call(selection) {
-        // console.log(this);
-        selection.each((data, index) => {
-            console.log("Generating chart");
-            // console.log(this);
-            console.log(`Width: ${this._width}`);
-            console.log(`Height: ${this._height}`);
-            console.log(data);
+        let outerThis = this;
+
+        // Graph width and height - accounting for margins
+        let drawWidth = outerThis._width - outerThis._margin.left - outerThis._margin.right;
+        let drawHeight = outerThis._height - outerThis._margin.top - outerThis._margin.bottom;
+
+        selection.each(function(data, index) {
+
+            let ele = d3.select(this);
+            let svg = ele.selectAll("svg").data([data]);
+
+            // Append static elements (i.e., only added once)
+            let svgEnter = svg.enter()
+                .append("svg")
+                .attr('width', outerThis._width)
+                .attr('height', outerThis._height);
+
+
+            let g = svgEnter.append('g')
+                .attr('transform', 'translate(' + outerThis._margin.left + ',' + outerThis._margin.top + ')')
+
+
+            let xAxisLabel = svgEnter.append('g')
+                .attr('transform', `translate(${outerThis._margin.left}, ${outerThis._margin.top + drawHeight})`)
+                .attr('class', 'axis')
+                // .call(xAxis);
+
+            let xAxisText = svgEnter.append('text')
+                .attr('transform', `translate(${outerThis._margin.left + drawWidth / 2}, ${outerThis._margin.top + drawHeight + 40})`)
+                .attr('class', 'axis-label')
+                .text(outerThis._xTitle);
+
+
+            let yAxisLabel = svgEnter.append('g')
+                .attr('class', 'axis')
+                .attr('transform', `translate(${outerThis._margin.left}, ${outerThis._margin.top})`)
+                // .call(yAxis);
+
+
+            let yAxisText = svgEnter.append('text')
+                .attr('transform', `translate(${outerThis._margin.left - 40}, ${outerThis._margin.top + drawHeight / 2}) rotate(-90)`)
+                .attr('class', 'axis-label')
+                .text(outerThis._yTitle);
         
         })
     }
