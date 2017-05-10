ccf1e3df060ade9bf93a8795e1ce43956e633504
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-10 15:12:06 -0700

    almost ready to do the data join

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ $(function () {
         let selection = d3.select("#viz").datum(data);
         // ES6 class reusable chart variation
         let chart = new TimeSeriesChart()
-            .width(500)
-            .height(500)
-            .call(selection)
+            // .width(500)
+            // .height(500)
+            .x(d => +d.date.slice(4))
+            .y(d => +d.price)
+            .call(selection);
         // Mike Bostock's closure reusable chart proposal
         // selection.call(chart.call);
 
--- reusable-time-series/js/time-series.js
+++ reusable-time-series/js/time-series.js
@@ -1,9 +1,13 @@
 class TimeSeriesChart {
     constructor() {
-        this._width = 200;
-        this._height = 100;
+        this._width = 960;
+        this._height = 500;
         this._xScale = d3.scaleLinear();
         this._yScale = d3.scaleLinear();
+        this._xFormat = d3.format("d");
+        this._yFormat = d3.format('.2s');
+        this._xValue = d => d;
+        this._yValue = d => d;
         this._xTitle = 'X AXIS TITLE';
         this._yTitle = 'Y AXIS TITLE';
 
@@ -25,6 +29,13 @@ class TimeSeriesChart {
         let drawHeight = outerThis._height - outerThis._margin.top - outerThis._margin.bottom;
 
         selection.each(function(data, index) {
+            // Convert data to standard representation greedily;
+            // this is needed for nondeterministic accessors.
+            data = data.map(function(d, i) {
+                return [outerThis._xValue(d, i), outerThis._yValue(d, i)];
+            });
+            console.log(data[0]);
+
 
             let ele = d3.select(this);
             let svg = ele.selectAll("svg").data([data]);
@@ -43,7 +54,6 @@ class TimeSeriesChart {
             let xAxisLabel = svgEnter.append('g')
                 .attr('transform', `translate(${outerThis._margin.left}, ${outerThis._margin.top + drawHeight})`)
                 .attr('class', 'axis')
-                // .call(xAxis);
 
             let xAxisText = svgEnter.append('text')
                 .attr('transform', `translate(${outerThis._margin.left + drawWidth / 2}, ${outerThis._margin.top + drawHeight + 40})`)
@@ -54,14 +64,41 @@ class TimeSeriesChart {
             let yAxisLabel = svgEnter.append('g')
                 .attr('class', 'axis')
                 .attr('transform', `translate(${outerThis._margin.left}, ${outerThis._margin.top})`)
-                // .call(yAxis);
-
 
             let yAxisText = svgEnter.append('text')
                 .attr('transform', `translate(${outerThis._margin.left - 40}, ${outerThis._margin.top + drawHeight / 2}) rotate(-90)`)
                 .attr('class', 'axis-label')
                 .text(outerThis._yTitle);
+                
         
+            // Create an ordinal color scale for coloring lines
+            let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+            // Define xAxis and yAxis functions
+            let xAxis = d3.axisBottom().tickFormat(this._xFormat);
+            let yAxis = d3.axisLeft().tickFormat(this._yFormat);
+
+            // Calculate x and y scales
+            let xExtent = d3.extent(data, d => d[0]);
+            outerThis._xScale.domain([xExtent[0], xExtent[1]]).rangeRound([0, drawWidth]);
+
+            let yExtent = d3.extent(data, d => d[1]);
+            outerThis._yScale.domain([yExtent[0], yExtent[1]]).rangeRound([drawHeight, 0]);
+
+            // colorScale.domain()
+
+            xAxis.scale(outerThis._xScale);
+            yAxis.scale(outerThis._yScale);
+
+            xAxisLabel.transition().duration(1000).call(xAxis);
+            yAxisLabel.transition().duration(1000).call(yAxis);
+
+            // // Update axes
+            // xAxis.scale(xScale);
+            // yAxis.scale(yScale);
+            // ele.select('.axis.x').transition().duration(1000).call(xAxis);
+            // ele.select('.axis.y').transition().duration(1000).call(yAxis);
+
         })
     }
 
@@ -75,6 +112,20 @@ class TimeSeriesChart {
     height(value) {
         if (value) {
             this._height = value;
+        }
+        return this;
+    }
+
+    x(_) {
+        if (_) {
+            this._xValue = _;
+        }
+        return this;
+    }
+
+    y(_) {
+        if (_) {
+            this._yValue = _;
         }
         return this;
     }
